cb74a07863aeb9ca365b2d824e5f22bf0905f2a9
ggvictor <victorrodriguessilva2@gmail.com>
2024-07-09 17:56:30 -0300

    adicionando novas funcionalidades ao javascrpit

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
 class BoxShadowGenerator {
-    constructor(horizontal,horizontalRef, vertical,verticalRef, blur,blurRef, spread, spreadRef,previewBox,rule,webKitRule,mozRule) {
+    constructor(horizontal,horizontalRef, vertical,verticalRef, blur,blurRef, spread,color,colorRef,opacity,opacityRef,inset, spreadRef,previewBox,rule,webKitRule,mozRule) {
 
-        this.horizontal= horizontal
-        this.horizontalRef= horizontalRef
-        this.vertical = vertical
-        this.verticalRef = verticalRef
-        this.blur = blur
-        this.blurRef = blurRef
-        this.spread = spread
-        this.spreadRef = spreadRef
-        this.previewBox = previewBox
-        this.rule = rule
-        this.webKitRule = webKitRule
-        this.mozRule = mozRule
+        this.horizontal= horizontal;
+        this.horizontalRef= horizontalRef;
+        this.vertical = vertical;
+        this.verticalRef = verticalRef;
+        this.blur = blur;
+        this.blurRef = blurRef;
+        this.spread = spread;
+        this.color = color;
+        this.colorRef = colorRef;
+        this.opacity = opacity;
+        this.opacityRef = opacityRef;
+        this.inset = inset ;
+        this.insetRef = inset.checked ;
+        this.spreadRef = spreadRef;
+        this.previewBox = previewBox;
+        this.rule = rule;
+        this.webKitRule = webKitRule;
+        this.mozRule = mozRule;
     }
 
     initialize(){
@@ -20,13 +26,20 @@ class BoxShadowGenerator {
         this.verticalRef.value = this.vertical.value;
         this.blurRef.value = this.blur.value;
         this.spreadRef.value = this.spread.value;
+        this.colorRef.value = this.color.value;
+        this.opacityRef.value = this.opacity.value;
+        
 
         this.applyRule();
         this.showRule();
     }
     applyRule(){
-        this.previewBox.style.boxShadow = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px  #000`;
-        this.currentRule = this.previewBox.style.boxShadow;
+        const rgbValue = this.hexToRgb(this.colorRef.value)
+
+        const shadowRule = `${this.insetRef ? "inset": ""} ${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px  rgba(${rgbValue}, ${this.opacityRef.value})`;
+
+        this.previewBox.style.boxShadow = shadowRule;
+        this.currentRule = shadowRule;
     }
     showRule(){
         this.rule.innerText = this.currentRule;
@@ -48,9 +61,22 @@ class BoxShadowGenerator {
             case "spread":
                 this.spreadRef.value = value;
                 break;
+            case "color":
+                this.colorRef.value = value;
+                break
+            case "opacity":
+                this.opacityRef.value = value;
+                break
+            case "inset":
+                this.insetRef = value;
+                break
         }
         this.applyRule();
         this.showRule();
+    }
+    // conversão rgb
+    hexToRgb(hex){
+        return `${("0x" + hex[1] + hex[2])| 0}, ${("0x" + hex[3] + hex[4] )| 0},${("0x" + hex[5] + hex[6] )| 0}`
     }
 }
 // Seleção de Elementos
@@ -62,6 +88,11 @@ const blur = document.querySelector("#blur")
 const blurRef = document.querySelector("#blur-value")
 const spread = document.querySelector("#spread")
 const spreadRef = document.querySelector("#spread-value")
+const color = document.querySelector("#color")
+const colorRef = document.querySelector("#color-value")
+const opacity = document.querySelector("#opacity")
+const opacityRef = document.querySelector("#opacity-value")
+const inset = document.querySelector("#inset")
 
 const previewBox = document.querySelector("#box")
 
@@ -69,8 +100,8 @@ const rule = document.querySelector("#rule span")
 const webKitRule = document.querySelector("#web-kit-rule span")
 const mozRule = document.querySelector("#moz-rule span")
 
-const boxShadow = new BoxShadowGenerator(horizontal,horizontalRef, vertical,verticalRef, blur,blurRef, spread, spreadRef,previewBox,rule,webKitRule,mozRule);
-
+const boxShadow = new BoxShadowGenerator(horizontal,horizontalRef, vertical,verticalRef, blur,blurRef, spread, color,colorRef,opacity,opacityRef,inset, spreadRef,previewBox,rule,webKitRule,mozRule);
+console.log(boxShadow)
 boxShadow.initialize();
 // Eventos
 horizontal.addEventListener("input", (e)=>{
@@ -92,4 +123,19 @@ spread.addEventListener("input", (e)=>{
     const value = e.target.value;
 
     boxShadow.uptadeValue("spread", value)
+})
+color.addEventListener("input", (e)=>{
+    const value = e.target.value;
+
+    boxShadow.uptadeValue("color", value)
+})
+opacity.addEventListener("input", (e)=>{
+    const value = e.target.value;
+
+    boxShadow.uptadeValue("opacity", value)
+})
+inset.addEventListener("input", (e)=>{
+    const value = e.target.checked;
+
+    boxShadow.uptadeValue("inset", value)
 })
